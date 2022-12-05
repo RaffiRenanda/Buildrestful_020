e63b35ee6650d90999a9453690c0486be4bfebc0
RaffiRenanda <raden.raffi.ft20@mail.umy.ac.id>
2022-12-05 08:44:19 +0700

    first commit

---
+++ src/main/java/pws/tutorialspoint/TutorialspointApplication.java
@@ -0,0 +1,13 @@
+package pws.tutorialspoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialspointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialspointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pws/tutorialspoint/TutorialspointApplicationTests.java
@@ -0,0 +1,13 @@
+package pws.tutorialspoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialspointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
